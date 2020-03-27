72a0da15ab37f61c7022acbf5e875d2ee10f65aa
Amit Pawar <pawaramitaaa@gmail.com>
2020-03-28 00:08:02 +0530

    board created

--- tiktakgame.sh
+++ tiktakgame.sh
@@ -1,2 +1,20 @@
 #!/bin/bash -x
-
+declare -A tiktaktoe
+function reset()
+{
+for((i=0;i<3;i++))
+do
+for((j=0;j<3;j++))
+do
+ tiktaktoe["$i,$j"]=1
+done
+done
+}
+reset
+function boarddisplay()
+{
+echo ${tiktaktoe["0,0"]} ${tiktaktoe["0,1"]} ${tiktaktoe["0,2"]}
+echo ${tiktaktoe["1,0"]} ${tiktaktoe["1,1"]} ${tiktaktoe["1,2"]}
+echo ${tiktaktoe["2,0"]} ${tiktaktoe["2,1"]} ${tiktaktoe["2,2"]}
+}
+print
